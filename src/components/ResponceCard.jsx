bd770e55b051df0861d2270c4a55cda485a10bc3
artem <artem.gusev@sheridancollege.ca>
2022-05-16 18:06:55 -0400

    Challenge completed
    in this last update added gradient color change to the AI model names in Results

--- src/components/ResponceCard.jsx
+++ src/components/ResponceCard.jsx
@@ -6,7 +6,7 @@ export default function ResponceCard( {prompt, reply, timestamp, engine} ) {
 		<Wrapper>
 			<h3 className='humanHeader'>Meaty Human Prompt:</h3>
 			<p>{prompt}</p>
-			<h3 className='aiheader'>{engine} reply:</h3>
+			<h3 className='aiheader'><span>{engine}</span> reply:</h3>
 			<p>{reply}</p>
 		</Wrapper>
 	);
@@ -25,6 +25,18 @@ const Wrapper = styled.div`
 	width: 100%;
 	background-color: #343a40;
 	border-radius: 30px;
+
+	span {
+		animation: colorChange 5s infinite;
+	}
+	@keyframes colorChange {
+		from{
+			filter: hue-rotate(360deg);
+		}
+		to{
+			transform: rotate(0deg);
+		}
+	}
 
     .aiheader, .humanHeader {
         font-size: 0.7rem;
